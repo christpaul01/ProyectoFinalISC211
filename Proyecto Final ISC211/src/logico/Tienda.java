3c4fbfd65fb9142eee643e7ab91c38fadccaa36e
joko49perez <joko49perez@x220t>
2018-11-15 19:45:48 -0400

    terminado los parametros de Tienda

--- Proyecto Final ISC211/src/logico/Tienda.java
+++ Proyecto Final ISC211/src/logico/Tienda.java
@@ -11,6 +11,65 @@ public class Tienda {
 	private ArrayList<Kit> kits;
 	private ArrayList<Usuario> usuarios;
 
+	public Tienda(ArrayList<Componente> componentes, ArrayList<Cliente> clientes, ArrayList<Factura> facturas,
+			ArrayList<Kit> kits, ArrayList<Usuario> usuarios) {
+		super();
+		this.componentes = componentes;
+		this.clientes = clientes;
+		this.facturas = facturas;
+		this.kits = kits;
+		this.usuarios = usuarios;
+	}	
+	
+	
+	public ArrayList<Cliente> getClientes() {
+		return clientes;
+	}
+
+
+
+	public void setClientes(ArrayList<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+
+
+
+	public ArrayList<Factura> getFacturas() {
+		return facturas;
+	}
+
+
+
+	public void setFacturas(ArrayList<Factura> facturas) {
+		this.facturas = facturas;
+	}
+
+
+
+	public ArrayList<Kit> getKits() {
+		return kits;
+	}
+
+
+
+	public void setKits(ArrayList<Kit> kits) {
+		this.kits = kits;
+	}
+
+
+
+	public ArrayList<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+
+
+	public void setUsuarios(ArrayList<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+
+
 	public ArrayList<Componente> getComponentes() {
 		return componentes;
 	}
