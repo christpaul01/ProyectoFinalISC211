09d858bd1308da02c8472fbf1fbffddfafaff140
domielias <45079270+domielias@users.noreply.github.com>
2018-11-15 16:20:45 -0400

    Clase cliente lista

---
+++ Proyecto Final ISC211/src/logico/Cliente.java
@@ -0,0 +1,34 @@
+package logico;
+
+public class Cliente {
+	private String nombre;
+	private String cedula;
+	private String direccion;
+	
+	public Cliente(String nombre, String cedula, String direccion) {
+		super();
+		this.nombre = nombre;
+		this.cedula = cedula;
+		this.direccion = direccion;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+
+}
