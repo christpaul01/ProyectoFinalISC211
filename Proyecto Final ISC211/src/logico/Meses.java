69177e4944d416e2ddfd1ef87336d8b67a42203f
Christopher <Christopher@Unknown-07>
2018-12-06 02:23:03 -0400

    Funcionalidad a todas las graficas. Calculo de ventas de cada componente

--- Proyecto Final ISC211/src/logico/Meses.java
+++ Proyecto Final ISC211/src/logico/Meses.java
@@ -11,18 +11,67 @@ public class Meses implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Ventas> ventas;
 	private String Mes;
+	private int cantRam;
+	private int cantMotherboard;
+	private int cantCPU;
+	private int cantHDD;
+	private int cantKits;
 	
 	public Meses() {
 		// TODO Auto-generated constructor stub
 		
 		ventas = new ArrayList<Ventas>();
-
+		cantRam = 0;
+		cantMotherboard = 0;
+		cantCPU = 0;
+		cantHDD = 0;
+		cantKits = 0;
 		
 	}
 	
 	public void insertarVentas(Ventas ventas)
 	{
 		this.ventas.add(ventas);
+		
+		for(int i = 0; i< ventas.getComponente().size() ;i++)
+		{
+			if(ventas.getComponente().get(i) instanceof Ram)
+			{
+				cantRam++;
+			}
+			
+			
+			if(ventas.getComponente().get(i) instanceof Motherboard)
+			{
+				cantMotherboard++;
+			}
+			
+			
+			
+			if(ventas.getComponente().get(i) instanceof Procesador)
+			{
+				cantCPU++;
+			}
+			
+			
+			if(ventas.getComponente().get(i) instanceof DiscoDuro)
+			{
+				cantHDD++;
+			}
+			
+			
+		}
+		
+		for(int i = 0; i< ventas.getKits().size() ;i++)
+		{
+			cantKits++;
+			//Ya que los kits incluyen obligatoriamente todos estos componentes
+			cantRam++;
+			cantMotherboard++;
+			cantCPU++;
+			cantHDD++;
+		}
+		
 	}
 
 	public ArrayList<Ventas> getVentas() {
@@ -35,6 +84,26 @@ public class Meses implements Serializable {
 
 	public void setMes(String mes) {
 		Mes = mes;
+	}
+
+	public int getCantRam() {
+		return cantRam;
+	}
+
+	public int getCantMotherboard() {
+		return cantMotherboard;
+	}
+
+	public int getCantCPU() {
+		return cantCPU;
+	}
+
+	public int getCantHDD() {
+		return cantHDD;
+	}
+
+	public int getCantKits() {
+		return cantKits;
 	}
 	
 	
--- Proyecto Final ISC211/src/logico/Tienda.java
+++ Proyecto Final ISC211/src/logico/Tienda.java
@@ -1,6 +1,5 @@
 package logico;
 
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -16,14 +15,13 @@ import java.util.Date;
 
 import javax.swing.JFileChooser;
 
-
 public class Tienda implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private ArrayList<Componente> componentes;
 	private ArrayList<Cliente> clientes;
 	private ArrayList<Factura> facturas;
@@ -32,14 +30,13 @@ public class Tienda implements Serializable {
 	private ArrayList<String> proveedores;
 	private static ArrayList<Meses> meses;
 	private static Meses mesActual;
-	
 
 	private String nombre;
 	private static Tienda tienda;
 
 	private Tienda() {
 		super();
-				
+
 		componentes = new ArrayList<Componente>();
 		clientes = new ArrayList<Cliente>();
 		facturas = new ArrayList<Factura>();
@@ -48,295 +45,247 @@ public class Tienda implements Serializable {
 		proveedores = new ArrayList<String>();
 		meses = new ArrayList<Meses>();
 		mesActual = new Meses();
-		
-		
-		//Funciones Extra
+
+		// Funciones Extra
 		mesActual.setMes(tiempoActual());
-		
-		char[] clave = {'a','d','m','i','n'};
-		
-		Administrador defecto = new Administrador("admin",clave,"admin",asignarIdUsuario()); // A cambiar
+
+		char[] clave = { 'a', 'd', 'm', 'i', 'n' };
+
+		Administrador defecto = new Administrador("admin", clave, "admin", asignarIdUsuario()); // A cambiar
 		insertarAdministrador(defecto);
 	}
-	
+
 	public static Tienda getInstance() {
-		
-		if(tienda!=null)
-		{
-			if(tiempoActual() != mesActual.getMes())
-			{
+
+		if (tienda != null) {
+			if (tiempoActual() != mesActual.getMes()) {
 				Meses miMes = new Meses();
 				miMes.setMes(tiempoActual());
 				meses.add(miMes);
 				mesActual.setMes(tiempoActual());
 			}
 		}
-		
-		
-		if(tienda==null)
-		{
+
+		if (tienda == null) {
 			tienda = new Tienda();
 		}
 		return tienda;
 	}
-	
+
 	public String asignarIdCliente() {
-		
-		String idConCeros = String.format("%06d", (clientes.size() +1 ));
-		
+
+		String idConCeros = String.format("%06d", (clientes.size() + 1));
+
 		return idConCeros;
-		
+
 	}
-	
+
 	public ArrayList<Meses> getMeses() {
 		return meses;
 	}
-	
+
 	public String asignarIdUsuario() {
-		
-		String idConCeros = String.format("%04d", (usuarios.size() +1 ));
-		
+
+		String idConCeros = String.format("%04d", (usuarios.size() + 1));
+
 		return idConCeros;
-		
+
 	}
-	
+
 	public String asignarSerial() {
-		
-		String idConCeros = String.format("%010d", (componentes.size() +1 ));
-		
+
+		String idConCeros = String.format("%010d", (componentes.size() + 1));
+
 		return idConCeros;
-		
+
 	}
-	
+
 	public String asignarIdKit() {
-		
-		String idConCeros = String.format("%04d", (kits.size() +1 ));
-		
+
+		String idConCeros = String.format("%04d", (kits.size() + 1));
+
 		return idConCeros;
-		
+
 	}
-	
-	public void insertarVendedor(Usuario vendedor)
-	{
+
+	public void insertarVendedor(Usuario vendedor) {
 		usuarios.add(vendedor);
 	}
-	
+
 	public void insertarCliente(Cliente cliente) {
 		clientes.add(cliente);
 	}
-	
-	public void insertarProducto(Componente componente)
-	{
+
+	public void insertarProducto(Componente componente) {
 		componentes.add(componente);
 	}
-	
+
 	public int calcRam() {
-		
+
 		int cant = 0;
-		
-		for(int i = 0; i< componentes.size(); i++)
-		{
-			if(componentes.get(i) instanceof Ram)
-				cant+=componentes.get(i).getCantidad();
+
+		for (int i = 0; i < componentes.size(); i++) {
+			if (componentes.get(i) instanceof Ram)
+				cant += componentes.get(i).getCantidad();
 		}
-		
+
 		return cant;
-		
+
 	}
-	
+
 	public int calcMotherBoard() {
-		
+
 		int cant = 0;
-		
-		for(int i = 0; i< componentes.size(); i++)
-		{
-			if(componentes.get(i) instanceof Motherboard)
-				cant+=componentes.get(i).getCantidad();
+
+		for (int i = 0; i < componentes.size(); i++) {
+			if (componentes.get(i) instanceof Motherboard)
+				cant += componentes.get(i).getCantidad();
 		}
-		
+
 		return cant;
-		
+
 	}
-	
+
 	public int calcCPU() {
-		
+
 		int cant = 0;
-		
-		for(int i = 0; i< componentes.size(); i++)
-		{
-			if(componentes.get(i) instanceof Procesador)
-				cant+=componentes.get(i).getCantidad();
+
+		for (int i = 0; i < componentes.size(); i++) {
+			if (componentes.get(i) instanceof Procesador)
+				cant += componentes.get(i).getCantidad();
 		}
-		
+
 		return cant;
-		
+
 	}
-	
+
 	public int calcHDD() {
-		
+
 		int cant = 0;
-		
-		for(int i = 0; i< componentes.size(); i++)
-		{
-			if(componentes.get(i) instanceof DiscoDuro)
-				cant+=componentes.get(i).getCantidad();
+
+		for (int i = 0; i < componentes.size(); i++) {
+			if (componentes.get(i) instanceof DiscoDuro)
+				cant += componentes.get(i).getCantidad();
 		}
-		
+
 		return cant;
-		
+
 	}
-	
-	
-	
-	
-	
+
 	public int indexCliente(String cedula) {
-		
+
 		boolean encontrado = false;
 		int i = 0;
-		
-		while(!encontrado && i < clientes.size())
-		{
-			if(clientes.get(i).getCedula().equals(cedula))
-			{
+
+		while (!encontrado && i < clientes.size()) {
+			if (clientes.get(i).getCedula().equals(cedula)) {
 				encontrado = true;
 			}
 			i++;
 		}
-		
+
 		return i;
 	}
-	
-	
-	public void insertarVendedor(Vendedor vendedor)
-	{
+
+	public void insertarVendedor(Vendedor vendedor) {
 		usuarios.add(vendedor);
 	}
-	
+
 	public int indexUsuario(String id) {
-		
+
 		int index = 0;
 		boolean encontrado = false;
-		while(!encontrado && index < usuarios.size())
-		{	
-			if(usuarios.get(index).getId().equalsIgnoreCase(id))
-			{
+		while (!encontrado && index < usuarios.size()) {
+			if (usuarios.get(index).getId().equalsIgnoreCase(id)) {
 				return index;
 			}
 			index++;
 		}
-		
+
 		return -1;
-		
+
 	}
 
-	
-	public void insertarAdministrador(Administrador admin)
-	{
+	public void insertarAdministrador(Administrador admin) {
 		usuarios.add(admin);
 	}
-	
 
-	
 	public ArrayList<Cliente> getClientes() {
 		return clientes;
 	}
-
-
 
 	public void setClientes(ArrayList<Cliente> clientes) {
 		this.clientes = clientes;
 	}
 
-
-
 	public ArrayList<Factura> getFacturas() {
 		return facturas;
 	}
-
-
 
 	public void setFacturas(ArrayList<Factura> facturas) {
 		this.facturas = facturas;
 	}
 
-
-
 	public ArrayList<Kit> getKits() {
 		return kits;
 	}
 
-
-
 	public void insertarKits(Kit kits) {
-		
+
 		this.kits.add(kits);
 	}
-	
-		
-
-
 
 	public ArrayList<Usuario> getUsuarios() {
 		return usuarios;
 	}
 
-
-
 	public void setUsuarios(ArrayList<Usuario> usuarios) {
 		this.usuarios = usuarios;
 	}
 
-
-
 	public ArrayList<Componente> getComponentes() {
 		return componentes;
 	}
- 
+
 	public void setComponentes(ArrayList<Componente> componentes) {
 		this.componentes = componentes;
 	}
-	
-	
-	
-	public void guardarDatos() throws IOException, ClassNotFoundException{
-		
-		
+
+	public void guardarDatos() throws IOException, ClassNotFoundException {
+
 		String url = urlEjecutable();
-		
+
 		new File(url).mkdir();
-		
-		
-		FileOutputStream foTienda = new FileOutputStream (url + "//MisDatos.dat");
-		ObjectOutputStream oosTienda	= new ObjectOutputStream (foTienda);
-		
+
+		FileOutputStream foTienda = new FileOutputStream(url + "//MisDatos.dat");
+		ObjectOutputStream oosTienda = new ObjectOutputStream(foTienda);
+
 		oosTienda.writeObject(tienda);
-		
+
 		foTienda.close();
-		
-	}
-	
-	public void cargarDatos() throws IOException, ClassNotFoundException{
-		
-		String url = urlEjecutable();
-		
-		FileInputStream fiTienda = new FileInputStream (url + "//MisDatos.dat");	
-		ObjectInputStream oisTienda = new ObjectInputStream(fiTienda);	
-			
-		tienda = (Tienda)oisTienda.readObject();
-		fiTienda.close();
-	
-	}
-	
-	public String urlEjecutable() {
-		
-		//Crea el String del link hacia el folder "Tienda" en 'Mis Documentos'
-		String url = new JFileChooser().getFileSystemView().getDefaultDirectory().toString() + "//Tienda" ;
-		
-		return url;
-		
+
 	}
 
+	public void cargarDatos() throws IOException, ClassNotFoundException {
+
+		String url = urlEjecutable();
+
+		FileInputStream fiTienda = new FileInputStream(url + "//MisDatos.dat");
+		ObjectInputStream oisTienda = new ObjectInputStream(fiTienda);
+
+		tienda = (Tienda) oisTienda.readObject();
+		fiTienda.close();
+
+	}
+
+	public String urlEjecutable() {
+
+		// Crea el String del link hacia el folder "Tienda" en 'Mis Documentos'
+		String url = new JFileChooser().getFileSystemView().getDefaultDirectory().toString() + "//Tienda";
+
+		return url;
+
+	}
 
 	public String getNombre() {
 		return nombre;
@@ -346,12 +295,10 @@ public class Tienda implements Serializable {
 		return proveedores;
 	}
 
-	public void insertarProveedor(String proveedor)
-	{
+	public void insertarProveedor(String proveedor) {
 		proveedores.add(proveedor);
 	}
-	
-	
+
 	public Meses getMesActual() {
 		return mesActual;
 	}
@@ -360,17 +307,27 @@ public class Tienda implements Serializable {
 		this.mesActual = mesActual;
 	}
 
-	public static String tiempoActual()
-	{
+	public static String tiempoActual() {
 		Date date = new Date();
 		LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 		int month = localDate.getMonthValue();
 		int year = localDate.getYear();
-		
-		String tiempoActual = new String(month + "-"+year);
-		
+
+		String tiempoActual = new String(month + "-" + year);
+
 		return tiempoActual;
-		
+
 	}
+
+	public int calcKits() {
+		int cant = 0;
+
+		for (int i = 0; i < kits.size(); i++) {
+			cant += kits.get(i).getCantidad();
+		}
+
+		return cant;
+	}
+
 
 }
